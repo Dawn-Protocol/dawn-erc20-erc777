b929ecb2a09d105d14854cccfc995f8a919eb5d3
Mikko Ohtamaa <mikko@opensourcehacker.com>
2020-03-05 22:40:48 +0100

    Testing, with massively happy with asserts

--- test/test.ts
+++
@@ -1,8 +0,0 @@
-// test/test.ts
-var assert = require('assert');
-
-describe('Array#join', () => {
-  it('joins all elements into a string with separator', () => {
-    assert(['a', 'b', 'c'].join(':') === 'a:b:c:');
-  });
-});
